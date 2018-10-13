ba181c3e87dbf6d85991a33422fad1b23e5f92e6
Michael Moghaddam <m.moghaddam385@outlook.com>
2018-10-13 12:59:37 -0700

    Simple message reciever endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
   "log"
   "fmt"
   "net/http"
@@ -20,6 +22,22 @@ func hello(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintln(w, "Hello World")
 }
 
+func recieveMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Message Recieved!!\n")
+	body, err := ioutil.ReadAll(r.Body)
+
+	if (err != nil) {
+		fmt.Printf("Error reading request body: %v", err)
+		return
+	}
+
+	bodyMap := make(map[string] string)
+
+	json.Unmarshal(body, &bodyMap)
+
+	fmt.Printf("Body: %v", bodyMap)
+}
+
 func main() {
   addr, err := determineListenAddress()
   if err != nil {
@@ -27,6 +45,7 @@ func main() {
   }
 
   http.HandleFunc("/", hello)
+  http.HandleFunc("/message", recieveMessage)
   log.Printf("Listening on %s...\n", addr)
   if err := http.ListenAndServe(addr, nil); err != nil {
     panic(err)
